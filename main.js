e00a6ac99c353120f8700a1b326bc8f21bc4fa18
Harris Junaid <harris.junaid@gmail.com>
2023-03-16 23:03:24 -0400

    17-6 update

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ window.addEventListener('load', ()=> {
 
 async function createBookList(){
     const response = await fetch('./books.json');
-    const json     = await response.json();
+    const json     = await response.json();                 console.log("JSON:"); console.log(json);
     const books    = document.getElementById('books');
 
-    json.books.forEach((book) => {
+    json.books.forEach((book) => {                          console.log("book:"); console.log(book);
         const element = document.createElement('mit-book');
         element.book = book;
         books.appendChild(element);
